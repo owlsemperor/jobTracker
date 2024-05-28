0ddfdfcd4b3f01df9cd05ce36bb4185c43453053
Mickey <mickeymanohar00@gmail.com>
2024-05-28 16:36:02 +0530

    fix update user image

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,22 +1,14 @@
 import multer from 'multer'
+import DataParser from 'datauri/parser.js'
 
 import path from 'path'
-const rootDir = process.cwd()
-const uploadDir = path.join(rootDir, 'public', 'uploads')
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // set the directory where uploaded files will be stored
-
-    cb(null, uploadDir)
-  },
-  filename: (req, file, cb) => {
-    const fileName = file.originalname
-    // set the name of the uploaded file
-    cb(null, fileName)
-  },
-})
-
-console.log(rootDir)
+const storage = multer.memoryStorage()
 const upload = multer({ storage })
+const parser = new DataParser()
+
+export const formatImage = (file) => {
+  const fileExtension = path.extname(file.originalname).toString()
+  return parser.format(fileExtension, file.buffer).content
+}
 
 export default upload
