cd3c145c272e71ed0be61c2b784aed29179a7539
Mickey <mickeymanohar00@gmail.com>
2024-05-28 15:42:17 +0530

    include build in fronted

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
-app.use(express.static(path.resolve(__dirname, './public')))
+app.use(express.static(path.resolve(__dirname, './client/dist')))
 
 app.use(cookieParser())
 app.use(express.json()) //JSON middleware
@@ -50,7 +50,7 @@ app.use('/api/v1/users', authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'))
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))
 })
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' })
